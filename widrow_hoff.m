5a7ea3c494dde40ed7143106731e93fa3afe8a5f
Bhaktipriya Shridhar <bhaktipriya96@gmail.com>
2017-01-29 13:32:24 +0530

    Widrow Hoff

--- widrow_hoff.m
+++ widrow_hoff.m
@@ -2,22 +2,26 @@ function [ a ] = widrow_hoff(Y , a, b, lr, theta)
 n = size(Y,1);
 iterations = 0;
 all_pos =0;
-while((all_pos ==0)&&(iterations<1000000))
+while((all_pos==0)&&(iterations<1000000))
     iterations = iterations+1; 
     fprintf('iteration=%d\n',iterations);
     all_pos = 1;
     for i = 1:n
-        if(Y(i, :)*a'+b <=0)
-            delta = lr*(Y(i,:))*((b(i)-Y(i,:)*a'));
-            a = a + delta;
-            if(abs(delta) > theta)
-            all_pos = 0;
+            if(isnan(b-Y(i,:)*a')||isinf(b-Y(i,:)*a'))
+               all_pos=1
+               break
             end
-            disp(a);
-        end
+            delta = (lr/iterations)*(Y(i,:))*((b-Y(i,:)*a'));
+            %a = a + delta;
+            if(delta > theta) 
+                a = a + delta;                
+                %disp(delta)
+                all_pos = 0;
+            end
+            %disp(a);
+        
     end
     if(all_pos==1)
-        disp('haha');
     disp(a);
     end
 end
